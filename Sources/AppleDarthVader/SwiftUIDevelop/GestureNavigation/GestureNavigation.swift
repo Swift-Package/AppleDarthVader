a2e816b76605671bfc33e88ef4e62236b84d3bd0
杨俊艺 <1209032479@qq.com>
2025-08-23 20:32:35 +0800

    sync

---
+++ Sources/AppleDarthVader/SwiftUIDevelop/GestureNavigation/GestureNavigation.swift
@@ -0,0 +1,22 @@
+//
+//  GestureNavigation.swift
+//  AppleDarthVader
+//
+//  Created by 杨俊艺 on 2025/8/22.
+//
+
+import UIKit
+import SwiftUI
+
+// MARK: - 解决自定义返回按钮导致侧滑返回手势失效的问题
+//extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+//    open override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        interactivePopGestureRecognizer?.delegate = self
+//    }
+//    
+//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        viewControllers.count > 1
+//    }
+//}
---
+++ Sources/AppleDarthVader/SwiftUIDevelop/Keyboard/ViewKeyboard.swift
@@ -0,0 +1,37 @@
+//
+//  ViewKeyboard.swift
+//  AppleDarthVader
+//
+//  Created by 杨俊艺 on 2025/8/21.
+//
+
+import SwiftUI
+
+public extension View {
+    // MARK: - 收起屏幕键盘
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+struct ViewKeyboardTestsView: View {
+    
+    @State private var text = ""
+    
+    var body: some View {
+        ZStack {
+            TextField("请输入文本", text: $text)
+                .padding()
+                .textContentType(.telephoneNumber)
+                .keyboardType(.phonePad)
+                .textFieldStyle(.roundedBorder)
+        }
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
+}
+
+#Preview {
+    ViewKeyboardTestsView()
+}
