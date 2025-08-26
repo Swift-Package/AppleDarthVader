a8039da7bb1ba5943bee87ea4e2580100d89f663
杨俊艺 <1209032479@qq.com>
2025-08-27 01:17:49 +0800

    sync

--- Sources/AppleDarthVader/SwiftUIDevelop/SwiftUIInteractUIKit/ViewExtract.swift
+++ Sources/AppleDarthVader/SwiftUIDevelop/SwiftUIInteractUIKit/ViewExtract.swift
@@ -9,6 +9,56 @@
 
 import SwiftUI
 
+struct ViewExtractView: View {
+    var body: some View {
+        TabView {
+            Tab.init("Home", systemImage: "heart.fill") {
+                VStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                        .viewExtract { view in
+                            print("这种View是SwiftUI原生的View不包含任何UIKit View") // 该行代码不会执行
+                        }
+                    
+                    Slider(value: .constant(2))
+                        .viewExtract { view in
+                            if let slider = view as? UISlider {
+                                slider.tintColor = .red
+                            }
+                        }
+                    
+                    TextField("Hello World", text: .constant(""))
+                        .viewExtract { _ in
+                            print("TextField 以及 Slider是使用SwiftUI包装器包装UIKit的")
+                        }
+                    
+                    List {
+                        Text("Hello World")
+                    }
+                    .viewExtract { view in
+                        if view is UICollectionView {
+                            print("SwiftUI 中的 List 是 UIKit 中的 UICollectionView")
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
+        .viewExtract { view in
+            if let tabViewController = view.next as? UITabBarController {
+                tabViewController.tabBar.isHidden = false
+                tabViewController.tabBar.tintColor = .red
+            }
+        }
+    }
+}
+
+#Preview {
+    ViewExtractView()
+}
+
 public extension View {
     @ViewBuilder
     func viewExtract(result: @escaping (UIView) -> Void) -> some View {
