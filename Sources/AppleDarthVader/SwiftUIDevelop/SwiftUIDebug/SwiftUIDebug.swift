3b944dd1e04ee9524041f4f41e21a312b390f04b
杨俊艺 <1209032479@qq.com>
2025-09-13 10:07:55 +0800

    添加调试时展示View边框的扩展

--- Sources/AppleDarthVader/SwiftUIDevelop/SwiftUIDebug/SwiftUIDebug.swift
+++ Sources/AppleDarthVader/SwiftUIDevelop/SwiftUIDebug/SwiftUIDebug.swift
@@ -12,4 +12,26 @@ public extension View {
         print(Mirror(reflecting: self).subjectType)
         return self
     }
+    
+    // MARK: - 在视图外围展示布局边框
+    func debugBorder(color: Color = .red, width: CGFloat = 1, cornerRadius: CGFloat = 0) -> some View {
+        self.modifier(DebugBorder(color: color, width: width, cornerRadius: cornerRadius))
+    }
+}
+
+// MARK: - 调试用显示布局边框
+private struct DebugBorder: ViewModifier {
+    
+    var color: Color = .red
+    var width: CGFloat = 1
+    var cornerRadius: CGFloat = 0
+    
+    func body(content: Content) -> some View {
+        #if DEBUG
+        content
+            .overlay(RoundedRectangle(cornerRadius: cornerRadius).stroke(color, lineWidth: width))
+        #else
+        content
+        #endif
+    }
 }
