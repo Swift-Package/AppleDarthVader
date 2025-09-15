dfe0ebb4a5509fe1bc2d317fbd11c58861741891
杨俊艺 <1209032479@qq.com>
2025-09-16 01:15:18 +0800

    sync

---
+++ Sources/ContinuousEvolution/SwiftUIEvolution/CustomLayut/AnyLayout/AlternateLayoutView.swift
@@ -0,0 +1,34 @@
+//
+//  AlternateLayoutView.swift
+//  AppleDarthVader
+//
+//  Created by 杨俊艺 on 2025/9/16.
+//
+
+// MARK: - 要看懂这个项目先查看 - AnyLayoutView 项目
+
+import SwiftUI
+
+//struct AlternateStackLayout: Layout {
+//    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+//        
+//    }
+//    
+//    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+//        
+//    }
+//}
+
+// MARK: - 交错布局视图
+struct AlternateLayoutView: View {
+    var body: some View {
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    AlternateLayoutView()
+}
---
+++ Sources/ContinuousEvolution/SwiftUIEvolution/CustomLayut/AnyLayout/AnyLayoutView.swift
@@ -0,0 +1,90 @@
+//
+//  AnyLayoutView.swift
+//  AppleDarthVader
+//
+//  Created by 杨俊艺 on 2025/9/16.
+//
+
+// AnyLayout and Custom Layouts in iOS 16 - https://www.youtube.com/watch?v=WD7ebJZ7PaI
+// Composing custom layouts with SwiftUI - Xcode 官方文档
+
+import SwiftUI
+
+// MARK: - 展示的色块数据
+struct MyColors {
+    
+    var color: Color
+    var width: CGFloat
+    var height: CGFloat
+    
+    static var allColors: [MyColors] {
+        [
+            .init(color: .red, width: 60, height: 75),
+            .init(color: .teal, width: 100, height: 100),
+            .init(color: .purple, width: 40, height: 80),
+            .init(color: .indigo, width: 120, height: 100),
+        ]
+    }
+}
+
+// MARK: - 三种不同的布局枚举
+enum LayoutType: Int, CaseIterable {
+    
+    case hStack
+    case vStack
+    case zStack
+    
+    var index: Int {
+        LayoutType.allCases.firstIndex(where: { $0 == self })!
+    }
+    
+    var layout: any Layout {
+        switch self {
+        case .hStack:
+            return HStackLayout(alignment: .top, spacing: 0)
+        case .vStack:
+            return VStackLayout(alignment: .trailing)
+        case .zStack:
+            return ZStackLayout(alignment: .center)
+        }
+    }
+}
+
+struct AnyLayoutView: View {
+    
+    @State private var layoutType = LayoutType.zStack
+    
+    var body: some View {
+        let layout = AnyLayout(layoutType.layout)
+        NavigationStack {
+            layout {
+                ForEach(MyColors.allColors, id:\.color) { mycolor in
+                    mycolor.color
+                        .frame(width: mycolor.width, height: mycolor.height)
+                    
+                }
+            }
+            .padding()
+            .navigationTitle("AnyLayout")
+            .animation(.default, value: layoutType)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        if layoutType.index < LayoutType.allCases.count - 1 {
+                            layoutType = LayoutType.allCases[layoutType.index + 1]
+                        } else {
+                            layoutType = LayoutType.allCases[0]
+                        }
+                    } label: {
+                        Image(systemName: "circle.grid.3x3.circle.fill")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AnyLayoutView()
+}
